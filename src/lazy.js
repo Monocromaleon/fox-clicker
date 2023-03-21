9576459bd191141a4860fe5ea257b51002b21979
Iván León Parada Marín <ivanparadamarin@gmail.com>
2023-03-21 16:36:40 -0500

    foxclicker

---
+++ src/lazy.js
@@ -0,0 +1,20 @@
+const isIntersecting = (entry) => {
+  return entry.isIntersecting;
+};
+
+const loadImage = (entry) => {
+  const container = entry.target;
+  const imagen = container.firstChild;
+  const url = imagen.dataset.src;
+  imagen.src = url;
+
+  observer.unobserve(container);
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.filter(isIntersecting).forEach(loadImage);
+});
+// funcion registrar imagen
+export const registerImage = (imagen) => {
+  observer.observe(imagen);
+};
